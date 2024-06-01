879e17b4efa8545cba3653f646650b12ef13cdb0
Garvit <taneja.garvit29@gmail.com>
2024-06-01 15:04:43 +0530

    utils

--- src/db/index.js
+++ src/db/index.js
@@ -3,8 +3,8 @@ import { DB_NAME } from "../constants.js";
 
 const connectDB = async()=>{
     try {
-        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-       console.log(`MongoDb connected and DB host:`)
+      const connectionInstance=   await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+       console.log(`MongoDb connected and DB host: ${connectionInstance.connection.host}`)
     } catch (error) {
         console.log("Error in connecting database",error)
     }
---
+++ src/utils/ApiResponse.js
@@ -0,0 +1,8 @@
+class ApiResponse {
+    constructor(statusCode, data = null, message = "success") {
+      this.statusCode = statusCode;
+      this.data = data;
+      this.message = message;
+      this.success = statusCode >= 200 && statusCode < 300; // success if status code is in the 2xx range
+    }
+}  
